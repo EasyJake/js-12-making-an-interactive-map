041bd4a0c83e0c7f70188e228e915e227a5fe806
EasyJake <EasyJake@users.noreply.github.com>
2023-10-15 21:38:40 -0700

    Made changes and added comments around the marker portion

--- script.js
+++ script.js
@@ -57,14 +57,38 @@ class TravelMap {
         this.userMarker.bindPopup("You are here!").openPopup();
     }
 
+    // addBusinessMarkers(businesses) {
+    //     this.removeBusinessMarkers();
+    //     businesses.forEach(business => {
+    //         let marker = L.marker(business.coords).addTo(this.map);
+    //         marker.bindPopup(business.name);
+    //         this.businessMarkers.push(marker);
+    //     });
+    // }
+
     addBusinessMarkers(businesses) {
+        // First, it calls a method to remove all current business markers from the map.
+        // This could be to ensure that the map is clean and only the new set of business markers are shown.
         this.removeBusinessMarkers();
+    
+        // It then iterates over the 'businesses' array, which is presumably a collection of business objects passed to this method.
         businesses.forEach(business => {
+            // For each 'business' object, a map marker is created at the business's coordinates.
+            // The 'L.marker' method is part of the Leaflet.js library, a popular open-source JavaScript library for interactive maps.
             let marker = L.marker(business.coords).addTo(this.map);
+    
+            // The marker is then bound with a popup that displays the business's name.
+            // So, when you click on a marker, a popup shows the name of the business at those coordinates.
             marker.bindPopup(business.name);
+    
+            // The 'marker' for each business is then added to the 'businessMarkers' array.
+            // This array probably exists as a property of the class to keep track of the markers currently displayed on the map.
+            // Storing these references allows the app to modify or remove the markers later, as seen with the 'removeBusinessMarkers' method call above.
             this.businessMarkers.push(marker);
         });
     }
+    
+    // easyJake
 
     removeBusinessMarkers() {
         this.businessMarkers.forEach(marker => this.map.removeLayer(marker));
